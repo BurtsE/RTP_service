e302e3108c05617ce3c7099a83461429e2c448dd
Ваше Имя <you@example.com>
2025-09-08 15:39:49 +0500

    log

--- internal/server/generation_handler.go
+++ internal/server/generation_handler.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -10,10 +11,11 @@ type GenerationResponseDTO struct {
 }
 
 func (s *HTTPServer) GenerationHandler(w http.ResponseWriter, r *http.Request) {
-
 	multiplicator := s.service.GenerateMultiplicator()
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(GenerationResponseDTO{Multiplicator: multiplicator})
+
+	log.Printf("generated multiplicator: %f", multiplicator)
 }
